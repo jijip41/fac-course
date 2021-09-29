863bf0d3113bc164065096f519f3fae5131e784e
jihye <jiji.pak41@gmail.com>
2021-09-29 10:44:47 +0100

    add scope-challenge

---
+++ week1-server/workshops/scope-challenge/challenge/burst.js
@@ -0,0 +1,40 @@
+let mousedown = false;
+
+document.addEventListener('mousedown', (event) => {
+  const { clientX, clientY } = event;
+  mousedown = true;
+
+  function createBurst(x, y) {
+    const el = document.createElement('div');
+    el.classList.add('burst');
+
+    const getRandomHue = randomRange(0, 360); // global?
+
+    const hue = getRandomHue();
+    el.style.setProperty('--hue', hue);
+
+    const top = x - 4;
+    const left = y - 4;
+    el.style.setProperty('top', top + 'px');
+    el.style.setProperty('left', left + 'px');
+
+    let size = 1;
+    function grow() {
+      size += 1;
+      el.style.setProperty('transform', `scale(${size})`);
+      if (mousedown) {
+        requestAnimationFrame(grow);
+      }
+    }
+
+    requestAnimationFrame(grow);
+    return el;
+  }
+
+  const burst = createBurst(clientX, clientY);
+  document.body.appendChild(burst);
+});
+
+document.addEventListener('mouseup', (event) => {
+  mousedown = false;
+});
---
+++ week1-server/workshops/scope-challenge/challenge/other.js
@@ -0,0 +1,3 @@
+// const requestAnimationFrame = () => {
+//   return '😈';
+// };
