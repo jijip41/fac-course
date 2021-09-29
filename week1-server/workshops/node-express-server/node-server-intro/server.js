b0912bf125f863020fd9839356d70982f4033596
jihye <jiji.pak41@gmail.com>
2021-09-29 23:23:16 +0100

    create and start a server

--- week1-server/workshops/node-express-server/node-server-intro/server.js
+++ week1-server/workshops/node-express-server/node-server-intro/server.js
@@ -1,2 +1,19 @@
+// Creating a server
+
 const express = require('express');
 const server = express();
+
+// Handling requests
+// -- add a route
+// call our function for any http get requests to our home path.
+// request: an object representing the incoming request
+// response: an object representing the response that will eventually be sent
+
+server.get('/', (request, response) => {
+  response.send('hello');
+});
+
+// Starting the server
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
