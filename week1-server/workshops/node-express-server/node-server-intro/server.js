3a8c79e0e2c320664092b3d5abf11784ddc12f06
jihye <jiji.pak41@gmail.com>
2021-09-30 00:24:46 +0100

    Middleware

--- week1-server/workshops/node-express-server/node-server-intro/server.js
+++ week1-server/workshops/node-express-server/node-server-intro/server.js
@@ -9,15 +9,16 @@ const server = express();
 // request: an object representing the incoming request
 // response: an object representing the response that will eventually be sent
 
-server.get('/', (request, response) => {
+server.get('/', logger, (request, response) => {
   // Status code
   // response.status(404);
 
   // Headers - Express will automatically set some headers describing the response. e.g. content-type, content-length
   // Use response.set method
-  response.set('x-fake-header', 'my-value');
-  const time = new Date().toLocaleDateString();
-  response.send(`<h1>Hello, it's ${time}</h1>`);
+
+  // response.set('x-fake-header', 'my-value');
+  // const time = new Date().toLocaleDateString();
+  // response.send(`<h1>Hello, it's ${time}</h1>`);
   response.send('hello');
 });
 
@@ -43,9 +44,27 @@ server.get('/users/:blah', (request, response) => {
 
 // Missing routes
 
-server.use((request, response) => {
-  response.status(404).send(`<h1>Not found</h1>`);
+// server.use((request, response) => {
+// response.status(404).send(`<h1>Not found</h1>`);
+// });
+
+// Middleware
+// Express route handlers don't have to send a response - they actually receive a third argument: the 'next' function
+// Calling the next function tells express to move on to the next handler registered for the route.
+
+// server.get('/', (request, response, next) => {
+//   console.log(request.method + ' ' + request.url);
+//   next();
+// });
+
+server.get('/', (request, response) => {
+  response.send(`<h1>Hello, this is the next handler</h1>`);
 });
+
+function logger(request, response, next) {
+  console.log(request.method + ' ' + request.url);
+  next();
+}
 
 // Starting the server
 
