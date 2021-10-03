93b9824eb55298e0f2b16e8cfbe2460fe6caab4c
jihye <jiji.pak41@gmail.com>
2021-10-03 22:37:50 +0100

    server-side forms search

--- week1-server/workshops/server-side-forms/workshop/server.js
+++ week1-server/workshops/server-side-forms/workshop/server.js
@@ -6,9 +6,15 @@ const dogs = require('./dogs.js');
 const server = express();
 
 server.get('/', (request, response) => {
+  searchItem = request.query.search || '';
   let items = '';
+
   for (const dog of Object.values(dogs)) {
-    items += `<li>${dog.name}</li>`;
+    if (searchItem.toLowerCase() === dog.name.toLowerCase()) {
+      items += `<li>${dog.name}</li>`;
+    } else if (searchItem === '') {
+      items += `<li>${dog.name}</li>`;
+    }
   }
   const html = `
   <!doctype html>
@@ -19,11 +25,26 @@ server.get('/', (request, response) => {
     </head>
     <body>
       <ul>${items}</ul>
+      <form>
+      <input name="search"></input>
+      <button>Search</button>
+      </form>
     </body>
   </html>
   `;
+
   response.send(html);
 });
+
+// GET requests
+
+// Browsers support two types of HTTP requests: GET and POST
+// When user navigates -> the browser will send a GET request, then render the response
+// Certain HTML tags trigger GET requests: <img> <form>...
+
+// By default a form send a GET request to the current page.
+// It will find all the inputs within the form and add them into the 'search' part of the URL(after '?')
+// Each input is added to the search string in this format : ${inpurName}=${inputValue}
 
 const PORT = 3333;
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
