80101900697926276eed297a4aa9fa3f926c5a4f
jihye <jiji.pak41@gmail.com>
2021-10-11 21:22:18 +0100

    reading a cookie with Express

--- week3/cookie-auth/workshop/server.js
+++ week3/cookie-auth/workshop/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 const server = express();
 
-server.get('/', (request, response) => {
-  // Reading a cookie
-  const cookies = request.get('cookie');
-  console.log(cookies);
-  response.send('<h1>Hello</h1>');
-});
+// server.get('/', (request, response) => {
+//   // Reading a cookie
+//   const cookies = request.get('cookie');
+//   console.log(cookies);
+//   response.send('<h1>Hello</h1>');
+// });
 
 // Setting a cookie
 // server.get('/example', (request, response) => {
@@ -21,6 +21,17 @@ server.get('/', (request, response) => {
 // Cookies with Express
 // Express' response object has a cookie method. - It takes three arguments (name, value, optional object for all the cookie options)
 
+// Reading a cookie with Express
+// Need to install cookie-parser and require it require('cookie-parser');
+// This middleware works like the built-in body-parsing one. It grabs the “cookie” header, parses it into a nice object, then attaches it to the request for you to use.
+
+server.use(cookieParser());
+
+server.get('/', (request, response) => {
+  console.log(request.cookies);
+});
+
+// Setting a cookie with Express
 server.get('/example', (request, response) => {
   response.cookie('Hi-Jihye', 'THis is your cookie', {
     httpOnly: true,
