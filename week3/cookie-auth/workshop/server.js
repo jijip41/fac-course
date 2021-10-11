59e152186cf5c735a4850a1a1dddeb6c2baa4aa5
jihye <jiji.pak41@gmail.com>
2021-10-11 21:09:12 +0100

    setting a cookie

--- week3/cookie-auth/workshop/server.js
+++ week3/cookie-auth/workshop/server.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
 
 const server = express();
 
-server.get("/", (request, response) => {
-  response.send("<h1>Hello</h1>");
+server.get('/', (request, response) => {
+  response.send('<h1>Hello</h1>');
+});
+
+// Setting a cookie
+server.get('/example', (request, response) => {
+  response.set(
+    'set-cookie',
+    "Hi=Jihye's cookie; HttpOnly; Max-Age=60; SameSite=Lax"
+  );
+  response.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000;
