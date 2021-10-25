1fa59ffc4156b2670797bf1ed68ce8008098eb20
jihye <jiji.pak41@gmail.com>
2021-10-25 22:15:08 +0100

    create UI with template

--- week5/dom-rendering/workshop/app.js
+++ week5/dom-rendering/workshop/app.js
@@ -9,38 +9,53 @@ const app = document.querySelector('#app');
 
 // const ul = document.createElement('ul');
 
-const dogList = dogs.map((dog) => {
-  // const h2 = createEl('h2', { textContent: dog.name });
-  // const h2 = document.createElement('h2');
-  // h2.textContent = dog.name;
+// const dogList = dogs.map((dog) => {
+// const h2 = createEl('h2', { textContent: dog.name });
+// const h2 = document.createElement('h2');
+// h2.textContent = dog.name;
 
-  // const image = createEl('img', {
-  //   src: dog.image,
-  //   alt: '',
-  //   width: 500,
-  //   height: 300,
-  // });
-  // const image = document.createElement('img');
-  // image.src = dog.image;
+// const image = createEl('img', {
+//   src: dog.image,
+//   alt: '',
+//   width: 500,
+//   height: 300,
+// });
+// const image = document.createElement('img');
+// image.src = dog.image;
 
-  // const li = createEl('li', {}, h2, image);
-  // const li = document.createElement('li');
-  // li.append(h2, image);
-  // console.log(li);
+// const li = createEl('li', {}, h2, image);
+// const li = document.createElement('li');
+// li.append(h2, image);
+// console.log(li);
 
-  return `<li>
-  <h2>${dog.name}</h2>
-  <img src="${dog.image}"></img>
-  </li>`;
-});
+//   return `<li>
+//   <h2>${dog.name}</h2>
+//   <img src="${dog.image}"></img>
+//   </li>`;
+// });
 
-app.innerHTML = `
-<h1>Dogs</h1>
-<ul>
-${dogList}
-</ul>
-`;
+// app.innerHTML = `
+// <h1>Dogs</h1>
+// <ul>
+// ${dogList}
+// </ul>
+// `;
 // const ul = createEl('ul', {}, ...dogList);
 // // ul.append(...dogList);
 
 // app.append(title, ul);
+
+// Challenge3- stretch
+// Template
+
+const template = document.querySelector('#homeTemplate');
+const clone = template.content.cloneNode(true);
+
+const dogList = dogs.map((dog) => {
+  return `<li><h2>${dog.name}</h2><img src="${dog.image}"></img></li>`;
+});
+
+clone.querySelector('.title').textContent = 'DOGS';
+clone.querySelector('.list-container').innerHTML = dogList;
+
+app.append(clone);
