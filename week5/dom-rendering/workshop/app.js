5b27a2f8c4276951faf6856e748a465ca3351625
jihye <jiji.pak41@gmail.com>
2021-10-25 21:40:29 +0100

    refactor app.js  with createEl function

--- week5/dom-rendering/workshop/app.js
+++ week5/dom-rendering/workshop/app.js
@@ -1,25 +1,31 @@
 import dogs from './dogs.js';
+import createEl from './create-element.js';
 
 const app = document.querySelector('#app');
 
-const title = document.createElement('h1');
-title.textContent = 'Dogs';
+const title = createEl('h1', {}, 'Dogs');
+// const title = document.createElement('h1');
+// title.textContent = 'Dogs';
 
-const ul = document.createElement('ul');
+// const ul = document.createElement('ul');
 
 const dogList = dogs.map((dog) => {
-  const li = document.createElement('li');
+  const h2 = createEl('h2', { textContent: dog.name });
+  // const h2 = document.createElement('h2');
+  // h2.textContent = dog.name;
 
-  const h2 = document.createElement('h2');
-  h2.textContent = dog.name;
+  const image = createEl('img', { src: dog.image });
+  // const image = document.createElement('img');
+  // image.src = dog.image;
 
-  const image = document.createElement('img');
-  image.src = dog.image;
-
-  li.append(h2, image);
+  const li = createEl('li', {}, h2, image);
+  // const li = document.createElement('li');
+  // li.append(h2, image);
+  console.log(li);
   return li;
 });
 
-ul.append(...dogList);
+const ul = createEl('ul', {}, ...dogList);
+// ul.append(...dogList);
 
 app.append(title, ul);
--- week5/dom-rendering/workshop/create-element.js
+++ week5/dom-rendering/workshop/create-element.js
@@ -1,4 +1,4 @@
-export function createEl(tag, props, ...children) {
+export default function createEl(tag, props, ...children) {
   const element = document.createElement(tag);
   for (const prop in props) {
     element[prop] = props[prop];
