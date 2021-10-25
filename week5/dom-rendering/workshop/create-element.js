aecdfcbbfe29e758ffa40f0145e7c591aa6441b0
jihye <jiji.pak41@gmail.com>
2021-10-25 21:30:59 +0100

    create createEL function and export it

---
+++ week5/dom-rendering/workshop/create-element.js
@@ -0,0 +1,8 @@
+export function createEl(tag, props, ...children) {
+  const element = document.createElement(tag);
+  for (const prop in props) {
+    element[prop] = props[prop];
+  }
+  element.append(...children);
+  return element;
+}
