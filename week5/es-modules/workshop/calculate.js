d8183fe0a409e33f2a82a9f0be30950e2e7b60f0
jihye <jiji.pak41@gmail.com>
2021-10-25 20:54:41 +0100

    create calculate.js and export it

---
+++ week5/es-modules/workshop/calculate.js
@@ -0,0 +1,15 @@
+import { add, subtract, multiply, divide } from './math.js';
+export function calculate(a, b, sign) {
+  switch (sign) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      throw new Error(`Sign '${sign}' not supported`);
+  }
+}
