10bcd68f3244d6eac05d10f42b154faeacf56070
jihye <jiji.pak41@gmail.com>
2021-10-26 23:34:29 +0100

    add price range and its state add key to list

--- week5/react-forms/workshop/App.jsx
+++ week5/react-forms/workshop/App.jsx
@@ -13,17 +13,45 @@ import dishes from '../data';
 // ];
 
 function App() {
+  const [min, setMin] = React.useState(0);
+  const [max, setMax] = React.useState(9);
   return (
     <main>
       <section className="filters">
         <h1>Burger Place</h1>
-        <form>Inputs go here</form>
+        <form>
+          <fieldset>
+            <legend>Price</legend>
+            <label htmlFor="min-price">
+              Min price
+              <input
+                type="range"
+                id="min-price"
+                min="0.5"
+                max="9"
+                step="0.25"
+                onChange={(e) => setMin(e.target.value)}
+              />
+            </label>
+            <label htmlFor="max-price">
+              Max price
+              <input
+                type="range"
+                id="max-price"
+                min="0.5"
+                max="9"
+                step="0.25"
+                onChange={(e) => setMax(e.target.value)}
+              />
+            </label>
+          </fieldset>
+        </form>
       </section>
       <section className="dishes">
         <h2>Dishes</h2>
         <ul className="grid">
           {dishes.map((dish) => (
-            <li className="card">
+            <li className="card" key={dish.id}>
               <h2>{dish.name}</h2>
               <p>{dish.description}</p>
               <p>£{dish.price.toFixed(2)}</p>
