7b5c3b3c71ec702271d1b66e53a9ae826e8cf9f0
jihye <jiji.pak41@gmail.com>
2021-12-30 00:22:39 +0000

    create and update data

--- week2/sql-intro/practice3.sql
+++ week2/sql-intro/practice3.sql
@@ -17,3 +17,17 @@ SELECT first_name, last_name, location FROM users WHERE age > 40 AND location = 
 -- 4. Select posts using multiple conditions 
 
 SELECT user_id, text_content FROM blog_posts WHERE user_id IN (2, 3);
+
+
+
+
+-- Challenge 2: Creating and updating data 
+
+-- 1. Adding a new post 
+
+INSERT INTO blog_posts (user_id, text_content) VALUES(1, 'Hello World') RETURNING user_id, text_content;
+
+
+-- 2. Updating an existing post
+
+UPDATE blog_posts SET user_id = 2 WHERE text_content = 'Hello World' RETURNING user_id;
