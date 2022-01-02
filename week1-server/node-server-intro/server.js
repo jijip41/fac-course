b48510e3d2424bff1cff5b5e2e4c1a115c2fd615
jihye <jiji.pak41@gmail.com>
2022-01-02 20:07:33 +0000

    set body with different types

--- week1-server/node-server-intro/server.js
+++ week1-server/node-server-intro/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const server = express();
 
 server.get('/', (request, response) => {
+  const time = new Date().toLocaleTimeString();
   response.set({
     'x-fake-header': 'my value',
     'x-another-header': 'another value',
   });
-  response.status(200).send('Hello');
+  response.status(200).send({
+    message: `<h1>Hello World</h1> <p>I'm Jihye</p><p>It's ${time}</p>`,
+  });
 });
 
 const PORT = 3000;
