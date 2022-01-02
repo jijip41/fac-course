4e4a48fcf0a007cae84b691574521529c670c0b4
jihye <jiji.pak41@gmail.com>
2022-01-02 19:57:00 +0000

    set response status

--- week1-server/node-server-intro/server.js
+++ week1-server/node-server-intro/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (request, response) => {
-  response.send('Hello');
+  response.status(404).send('Hello');
 });
 
 const PORT = 3000;
