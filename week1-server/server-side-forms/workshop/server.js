3d50f20d55b378cc7e79a12253095e863d42338e
jihye <jiji.pak41@gmail.com>
2022-01-06 00:31:39 +0000

    add comments for my understanding of forms and GET requests

--- week1-server/server-side-forms/workshop/server.js
+++ week1-server/server-side-forms/workshop/server.js
@@ -9,11 +9,22 @@ server.get('/', (request, response) => {
   let list = '';
   for (const dog of Object.values(dogs)) {
     const match = dog.name.toLowerCase().includes(search.toLowerCase());
-    // if we don't have a search submission we show all dogs
+
     if (match || !search) {
       list += `<li>${dog.name}</li>`;
     }
   }
+
+  // Forms can make GET requests
+  // By default a form sends a GET request to the current page when submitted
+  // Form will find all the inputs within the form and add them into the “search” part of the URL
+  // e.g. GET example.com?myMessage=whatevertheusertyped HTTP/1.1
+  // the bit after ? is a “search” part of the URL
+  // Each input is added to the search string in this format: ${inputName}=${inputValue}
+  // !!!! need a name attribute for input !!!!
+
+  // Express automatically parses the “search” part of the URL for each request. You can access this object at request.query
+
   const html = `
   <!doctype html>
   <html>
